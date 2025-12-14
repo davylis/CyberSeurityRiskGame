40b945aed0d8243982b1ead5bbd1c9bd99bfd659
Liisa Davydov <liisamuttonen@gmail.com>
2025-12-15 01:09:16 +0200

    Add raport scene and modify playinfo scene

--- CyberMaze/Assets/Scripts/View/CameraFollow.cs
+++ CyberMaze/Assets/Scripts/View/CameraFollow.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
-    public float smoothness = 2f;
+    public float smoothness = 1.5f;
     void Update()
     {
-        Vector3 targetPos = new Vector3(player.position.x, player.position.y, transform.position.z);
+        Vector3 targetPos = new Vector3(player.position.x + 3, player.position.y + 1, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPos, smoothness * Time.deltaTime);
     }
 }
