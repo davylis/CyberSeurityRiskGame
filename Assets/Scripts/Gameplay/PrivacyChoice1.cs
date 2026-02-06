97fb0e2b2c567c5d816c218d68c41faa72268339
Liisa Davydov <liisamuttonen@gmail.com>
2026-02-06 14:50:59 +0200

    Modify PrivacyPolicy scene for a task

---
+++ Assets/Scripts/Gameplay/PrivacyChoice1.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PrivacyChoice : MonoBehaviour
+{
+    public GameObject privacyPanel;
+    public GameObject phonePanel;
+    public GameObject goodPanel;
+    public GameObject badPanel;
+
+    public void Agree()
+    {
+        privacyPanel.SetActive(false);
+        phonePanel.SetActive(false);
+        goodPanel.SetActive(true);
+    }
+
+    public void Disagree()
+    {
+        privacyPanel.SetActive(false);
+        phonePanel.SetActive(false);
+        badPanel.SetActive(true);
+    }
+}
